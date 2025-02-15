98e99d7b8179c1d231d41f99dbef55050f4ca3ce
sanjivcdac <sanjivcdac@yahoo.com>
2025-02-14 18:56:55 -0600

    Library

---
+++ awesome-library/src/main/java/com/devcraze/awesome_library/AwesomeLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.devcraze.awesome_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeLibraryApplication.class, args);
+	}
+
+}
---
+++ awesome-library/src/test/java/com/devcraze/awesome_library/AwesomeLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcraze.awesome_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
